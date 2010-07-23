fb70686e611e4890769440d219ac0f3728063be1
setmaat <setmaat@018abfe8-e4ad-d6e2-5340-d55328ea0f90>
2010-07-23 00:29:53 +0000

    Share project "com.cblformatter.ui" into "https://sorca.googlecode.com/svn/trunk"

---
+++ sorcaRCP/com.cblformatter.ui/src/com/cblformatter/views/ProcessView.java
@@ -0,0 +1,152 @@
+package com.cblformatter.views;
+import java.io.File;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.NotEnabledException;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+
+import com.cblformatter.handler.CreateOutput;
+import com.cblformatter.handler.FillLinesHandler;
+import com.cblformatter.model.beans.Model;
+import com.cblformatter.utils.Costants;
+import com.cblformatter.views.utils.GuiUtils;
+
+
+public class ProcessView extends ViewPart {
+	protected static final String ID = "CBLFormatter.ProcessView";;
+	private Group grpProcess;
+	private Composite top;
+	private Button output;
+	private Button input;
+	private Button process;
+
+	public ProcessView() {
+
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+
+		GridLayout parentLayout = new GridLayout();
+		parentLayout.makeColumnsEqualWidth = true;
+		parent.setLayout(parentLayout);
+		top = new Composite(parent, SWT.NONE);
+		GridLayout composite1Layout = new GridLayout();
+		composite1Layout.makeColumnsEqualWidth = true;
+		GridData topLData = new GridData();
+		topLData.horizontalAlignment = GridData.CENTER;
+		topLData.grabExcessHorizontalSpace = true;
+		topLData.grabExcessVerticalSpace = true;
+		top.setLayoutData(topLData);
+		top.setLayout(composite1Layout);
+
+		grpProcess = new Group(top, SWT.NONE);
+		GridLayout grpProcessLayout = new GridLayout();
+		grpProcessLayout.numColumns = 3;
+		grpProcess.setLayout(grpProcessLayout);
+		GridData grpProcessLData = new GridData();
+		grpProcessLData.horizontalAlignment = GridData.CENTER;
+
+		grpProcess.setLayoutData(grpProcessLData);
+		grpProcess.setText("Genera CBL");
+
+		process = new Button(grpProcess, SWT.NONE );
+		GridData btnInputLData = new GridData();
+		process.setLayoutData(btnInputLData);
+		process.setText("Genera");
+		process.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent evt) {
+				
+				try {
+					GuiUtils.getHandlerService(ProcessView.ID).executeCommand(FillLinesHandler.ID, null);
+			
+				
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotDefinedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotEnabledException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotHandledException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+//				 IViewPart view = PlatformUI.getWorkbench()
+//			        .getActiveWorkbenchWindow()
+//			        .getActivePage()
+//			        .findView("CBLFormatter.FilesView");
+//			    if (view instanceof FilesView) {
+//	
+//			    	String filePath = ((FilesView) view).getFilePath().getText();
+//			    	String folderPath = ((FilesView) view).getFolderPath().getText();
+//			    	
+//			    	if(filePath.equals("none") || folderPath.equals("none")){
+//			    		MessageDialog.openError(null, "Errore", "Devi selezionare un file di input e una cartella di destinazione");
+//			    	}else{
+//
+//			    	File fileInput = new File(filePath);
+//			    	File fileOutput = new File(folderPath);
+//
+//				   // CreateOutput.createOutputFile(fileInput,fileOutput,Costants.input);
+//				    
+//			    	GuiUtils.showInfo("test", SWT.NONE);
+//				    PreView dialog = new PreView( PlatformUI.getWorkbench().getDisplay().getActiveShell());
+//				    dialog.open();
+//				    
+//				    System.out.println("File Input Salvato con successo");
+//			    	}
+//			    }
+
+			}
+		});
+
+		input = new Button(grpProcess, SWT.CHECK);
+		GridData btnOutputLData = new GridData();
+		input.setLayoutData(btnOutputLData);
+		input.setText("CBL Input");
+
+
+		output = new Button(grpProcess, SWT.CHECK );
+		GridData btnBothLData = new GridData();
+		output.setLayoutData(btnBothLData);
+		output.setText("CBL Output");
+
+
+		
+		bindGUI();
+
+	}
+
+	private void bindGUI() {
+
+		GuiUtils.addBindingContext(
+				input,Model.getSettingsBean(), "generateInput");
+		GuiUtils.addBindingContext(
+				output,Model.getSettingsBean(), "generateOutput");
+	}
+
+	@Override
+	public void setFocus() {
+
+
+	}
+
+}
---
+++ sorcaRCP/com.cblformatter.ui/src/com/cblformatter/views/utils/GuiUtils.java
@@ -0,0 +1,119 @@
+package com.cblformatter.views.utils;
+
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.BeansObservables;
+import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.IHandlerService;
+import org.eclipse.ui.part.ViewPart;
+
+import com.cblformatter.model.beans.ModelObject;
+
+public class GuiUtils {
+
+	
+	public static ViewPart getView(String ID){
+		ViewPart view = null;
+		try{
+			view = (ViewPart) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findView(ID);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return view;
+	}
+
+	public static Device getDisplay() {
+		return PlatformUI.getWorkbench().getDisplay();
+	}
+
+	public static void showInfo(String message, int style) {
+		MessageBox msg = new MessageBox(new Shell(),style);
+		msg.setMessage(message);
+		msg.open();
+		
+	}
+
+//	public static TableViewer getManagerTableViewer() {
+//		// TODO Auto-generated method stub
+//		return 	((ManagerView)GuiUtils.getView(ManagerView.ID)).getTv();
+//	}
+
+	public static boolean showConfirmDialog(String message) {
+		boolean ret =  MessageDialog.openConfirm(new Shell(), "Confirm", message);
+
+		return ret;
+	}
+
+	public static IHandlerService getHandlerService(String viewID) {
+		try {
+		IHandlerService handlerService = (IHandlerService) GuiUtils.getView(viewID).getSite()
+			.getService(IHandlerService.class);
+
+		return handlerService;
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	public static void addBindingContext(Control widget, ModelObject bean, String valueToBind) {
+		
+		if(widget instanceof Button ){
+			
+			DataBindingContext dbc = new DataBindingContext();
+			dbc.bindValue(SWTObservables.observeSelection(widget),
+					BeansObservables.observeValue(bean,valueToBind),null,null);
+			 
+			}
+		
+		if(widget instanceof Combo){
+		
+		DataBindingContext dbc = new DataBindingContext();
+		 dbc.bindValue(SWTObservables.observeSelection(widget),
+			 BeansObservables.observeValue(bean,valueToBind),null,null);
+		 
+		}
+		
+		if(widget instanceof Label ){
+			
+			DataBindingContext dbc = new DataBindingContext();
+			dbc.bindValue(SWTObservables.observeText(widget),
+					BeansObservables.observeValue(bean,valueToBind),null,null);
+			 
+			}
+		
+		
+		if(widget instanceof Text ){
+			
+			DataBindingContext dbc = new DataBindingContext();
+			dbc.bindValue(SWTObservables.observeText(widget,SWT.Modify),
+					BeansObservables.observeValue(bean,valueToBind),null,null);
+			 
+			}
+		
+		
+	}
+
+	public static void showError(String string) {
+		MessageDialog.openError(new Shell(), "Confirm", string);
+	}
+
+	public static void showInfo(String message, String string) {
+		showInfo(message,SWT.NONE);
+	}
+	
+}
