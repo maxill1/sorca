73e051006bef35e59f97db66c85a1845ccf00b4a
setmaat <setmaat@018abfe8-e4ad-d6e2-5340-d55328ea0f90>
2010-07-26 13:54:11 +0000

    Share project "com.cblformatter.ui" into "https://sorca.googlecode.com/svn/trunk"

---
+++ sorcaRCP/com.cblformatter.ui/src/com/cblformatter/handler/CreateOutputOperation.java
@@ -0,0 +1,21 @@
+package com.cblformatter.handler;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+
+public class CreateOutputOperation implements IRunnableWithProgress {
+
+	@Override
+	public void run(IProgressMonitor monitor) throws InvocationTargetException,
+			InterruptedException {
+		// TODO Auto-generated method stub
+
+
+		
+		System.out.println("test");
+		
+	}
+
+}
